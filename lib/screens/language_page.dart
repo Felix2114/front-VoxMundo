ac287cea8e92044c5c1911e9cec31b742d633be4
Felix2114 <zS23004696@estudiantes.uv.mx>
2025-05-21 20:38:50 -0600

    update settings

---
+++ lib/screens/language_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LanguagePage extends StatelessWidget {
+  const LanguagePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Idioma')),
+      body: const Center(child: Text('Aquí va la configuración de idioma')),
+    );
+  }
+}
